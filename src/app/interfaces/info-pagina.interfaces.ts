50367782d974a381095e3b15576ae3eb33fc9fca
Jose Sobrero <josesobrero25@hotmail.com>
2019-02-07 11:22:21 -0300

    Fin Seccion 4

---
+++ src/app/interfaces/info-pagina.interfaces.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombreCorto?: string;
+  paginaAutor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tubmlr?: string;
+  equipoTrabajo?: any[];
+}
